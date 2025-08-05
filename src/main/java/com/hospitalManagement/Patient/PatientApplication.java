1af2739c95a8cd3b272edb7ca9beb01ed49d7b4b
lovelyrajeshji <101453433+lovelyrajeshji@users.noreply.github.com>
2025-08-05 07:51:15 +0530

    Update PatientApplication.java

--- src/main/java/com/hospitalManagement/Patient/PatientApplication.java
+++ src/main/java/com/hospitalManagement/Patient/PatientApplication.java
@@ -9,13 +9,5 @@ public class PatientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PatientApplication.class, args);
 	}
-	@Bean
-	public String hotfixBean(){
-<<<<<<< HEAD
-		return "Hotfix from feature";
-=======
-		return "Hotfix from feature to main";
->>>>>>> feature
-	}
 
 }
